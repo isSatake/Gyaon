53dbfb1093ecf465423aea4ad1b19dfea528ae69
stkay <stkayy@gmail.com>
2016-11-11 18:46:49 +0900

    変数名変更

--- routes/index.js
+++ routes/index.js
@@ -11,8 +11,8 @@ var debug       = require("debug")("index");
 var model       = require('../model/model');
 var formatDate  = require('../util/formatdate');
 
-var endPoint = process.env.BASE_URL || 'http://localhost:3000';
-var strageEndPoint = 'https://s3-us-west-2.amazonaws.com/gyaon';
+var endPoint    = process.env.BASE_URL || 'http://localhost:3000';
+var s3EndPoint  = 'https://s3-us-west-2.amazonaws.com/gyaon';
 
 //create tmp folder
 var promiseUploadDir = function() {
@@ -55,7 +55,7 @@ router.get('/:id', function(req, res, next) {
 router.get('/sounds/:id/:name:ext(.wav|.mp3)?', function(req, res){
   var gyaonId = req.params.id;
   var fileName = req.params.name;
-  res.redirect(strageEndPoint + "/" + gyaonId + "/" + fileName);
+  res.redirect(s3EndPoint + "/" + gyaonId + "/" + fileName);
 });
 
 /* 音声データ受け取り */
