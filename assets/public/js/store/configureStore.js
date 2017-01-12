0bcef2e915cd7f50525d2983a0f3355aa6e34f95
stkay <stkayy@gmail.com>
2017-01-12 11:23:57 +0900

    settings for production

--- assets/public/js/store/configureStore.js
+++ assets/public/js/store/configureStore.js
@@ -1,14 +1,28 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import rootReducer from '../reducers'
 import { composeWithDevTools } from 'redux-devtools-extension' /* Redux開発用ChromeExtension */
 import { requestPermission, recorder, uploader } from '../middlewares/recordUpload'
 
 export default function configureStore(preloadState){
+  if (process.env.NODE_ENV === 'production') {
+    return createStore(
+      rootReducer,
+      preloadState,
+      composeWithDevTools(
+        applyMiddleware(
+          thunk,
+          requestPermission,
+          recorder,
+          uploader,
+        )
+      )
+    )
+  }
   return createStore(
     rootReducer,
     preloadState,
-    composeWithDevTools(
+    compose(
       applyMiddleware(
         thunk,
         requestPermission,
